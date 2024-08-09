6d3db924311e1e0735cc8380607b72cf5a88f730
Luis Mera <profeluismeradev@gmail.com>
2024-08-08 21:01:35 -0300

    clase 10

--- Clase 8 - Arquitectura por capas/backend/src/config/envs.config.js
+++ Clase 8 - Arquitectura por capas/backend/src/config/envs.config.js
@@ -1,6 +1,10 @@
 import dotenv from "dotenv";
 
-dotenv.config();
+const environment = "PRODUCTION";
+
+dotenv.config({
+  path: environment === "PRODUCTION" ? "./.env.prod" : "./.env.dev",
+});
 
 export default {
   PORT: process.env.PORT,
@@ -8,5 +12,5 @@ export default {
   SECRET_CODE: process.env.SECRET_CODE,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  JWT_SECRET_CODE:process.env.JWT_SECRET_CODE
+  JWT_SECRET_CODE: process.env.JWT_SECRET_CODE,
 };
